7d5d214cc720d261cee0f2aceaacf6e9f875d910
aca10jl <aca10jl@users.noreply.github.com>
2018-06-30 16:54:17 +0100

    Order data by date & time

--- Analysis.R
+++ Analysis.R
@@ -11,14 +11,16 @@ usd <- usd[!duplicated(usd), ]
 rownames(gbp) <- 1:nrow(gbp)
 rownames(usd) <- 1:nrow(usd)
 
+# sort the two dataframe by date and time in increasing order
+gbp$发布时间 <- as.POSIXct(gbp$发布时间, format = "%Y.%m.%d %H:%M:%S", tz = "GMT")
+usd$发布时间 <- as.POSIXct(usd$发布时间, format = "%Y.%m.%d %H:%M:%S", tz = "GMT")
+gbp <- gbp[order(gbp$发布时间, decreasing = FALSE), ]
+usd <- usd[order(usd$发布时间, decreasing = FALSE), ]
+
+# plot
+plot(gbp$现汇卖出价, type = "l", col = "red", xlab = "Time", ylab = "GBP", main = "Bank of China Currency") # ylim=c(600,900)
+plot(usd$现汇卖出价, type = "l", col = "blue", xlab = "Time", ylab = "USD", main = "Bank of China Currency")
+
 # save processed data back into original files
 write.csv(gbp, file = "GBP.csv", row.names = FALSE)
 write.csv(usd, file = "USD.csv", row.names = FALSE)
-
-# sort the two data by row names in decreasing order
-gbp <- gbp[order(as.numeric(rownames(gbp)), decreasing = TRUE), ]
-usd <- usd[order(as.numeric(rownames(usd)), decreasing = TRUE), ]
-
-# plot
-plot(gbp$现汇卖出价, type="l", col="red") # ylim=c(600,900)
-plot(usd$现汇卖出价, type="l", col="blue")
